a77f884916dd9cbae9bdc54457786a2cc9a68a70
Pablo Fernandez <heelhook@littleq.net>
2012-12-26 01:45:45 -0300

    Support to save contributors

--- lib/frisky-mongo/models/repository.rb
+++ lib/frisky-mongo/models/repository.rb
@@ -18,10 +18,22 @@ module Frisky
       key :full_name, String
       key :name, String
       key :url, String
+      key :contributors, Object
 
       timestamps!
 
       belongs_to :owner, class_name: 'Frisky::Model::Person'
+
+      def save(*args)
+        if self.no_proxy_contributors
+          cvalues = {}
+          self.no_proxy_contributors.each do |c, value|
+            cvalues[c.to_s] = value
+          end
+          self.contributors = cvalues
+        end
+        super(*args)
+      end
     end
   end
 end
